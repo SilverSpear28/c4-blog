a19cea217e889e7aed3ddcc5ea8c2ccdec222836
SilverSpear28 <abhijaymessi@gmail.com>
2020-04-17 12:10:52 +0530

    nextSlide function added

--- src/components/carosel.js
+++ src/components/carosel.js
@@ -18,11 +18,18 @@ class Carousel extends React.Component{
         const lastIndex = this.imgUrl.length - 1;
         const { currentImgIndex } = this.state;
         const shouldResetIndex = currentImgIndex === 0;
-        const index = shouldResetIndex ? lastIndex : currentImgIndex;
+        const index = shouldResetIndex ? lastIndex : currentImgIndex - 1;
 
         this.setState({
             currentImgIndex: index
         });
+    }
+
+    nextSlide = () => {
+        const lastIndex = this.imgUrl.length - 1;
+        const { currentImgIndex } = this.state;
+        const shouldResetIndex = currentImgIndex === lastIndex;
+        const index = shouldResetIndex ? 0 : currentImgIndex + 1 ;
     }
 
     render(){
