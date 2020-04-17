c65a11e173722de8e4d4bb999594b8a78c602f0b
SilverSpear28 <abhijaymessi@gmail.com>
2020-04-17 18:57:49 +0530

    add popular-post

--- src/components/second-blog.js
+++ src/components/second-blog.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import SecondBlogLeft from './second-blog-left'
 import MorePost from './morepost'
+import InfoBox from './c4-bio-box'
+import Popular from './popular-post'
 
 class SecondBlog extends React.Component{
     constructor(props){
@@ -16,23 +18,15 @@ class SecondBlog extends React.Component{
                 <div className='c4-second-blog-details'>
                     <SecondBlogLeft />
                     <div className='c4-second-blog-right'>
-                        <div className='c4-bio-box'>
-                            <img src="https://colorlib.com/preview/theme/balita/images/person_1.jpg" alt="Image Placeholder" />
-                            <div className="bio">
-                                <h2>Hinata Hyuga</h2>
-                                <p>
-                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem facilis sunt repellendus excepturi beatae porro debitis voluptate nulla quo veniam fuga sit molestias minus.
-                                </p>
-                                <p>
-                                    <button className='explore-bio'>Read my bio</button>
-                                </p>
-                            </div>
-                        </div>
+                        <InfoBox />
+                        <Popular />
                     </div>
                     </div>
                     <div className='c4-second-blog-head'>
                     <h1>More Blog Posts</h1>
                     <div className='c4-more-blog-post'>
+                        <MorePost />
+                        <MorePost />
                         <MorePost />
                     </div>
                 </div>
