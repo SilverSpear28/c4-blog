37373acc6d2c9f7cef2bf6c875485d3a76bc75cd
SilverSpear28 <abhijaymessi@gmail.com>
2020-04-17 19:43:23 +0530

    add footer

--- src/pages/blog.jsx
+++ src/pages/blog.jsx
@@ -10,6 +10,7 @@ class Blogs extends React.Component{
 
     render(){
         return(
+            <div>
             <div className='c4-blog'>
                 <div className='c4-blog-heading'>
                     <p>
@@ -23,6 +24,28 @@ class Blogs extends React.Component{
                     <MainCard />
                 </div>
                 <SecondBlog />
+            </div>
+            <div className='c4-footer'>
+                <div className='footer-container'>
+                    <div className='paragraph'>
+                    <h3>PARAGRAPH</h3>
+                    <img src="https://i0.wp.com/www.universodegatos.com/wp-content/uploads/2017/04/fivfelv7.jpg?resize=582%2C328" />
+                    <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi, accusantium optio unde perferendis eum illum voluptatibus dolore tempora, consequatur minus asperiores temporibus reprehenderit.
+                    </p>
+                    </div>
+                    <div className='latest'>
+                        <div className='latest-posts'>
+                            <h3>LATEST POSTS</h3>
+                        </div>
+                        <div className='links-social'>
+                        <div className='links'>
+                        <h3>QUICK LINKS</h3>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+            </div>
             </div>
         )
     }
